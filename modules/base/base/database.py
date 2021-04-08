c7c4a63df44065ae5014e4efc9e94de6ab6e1d51
sinus-x <14353790+sinus-x@users.noreply.github.com>
2021-04-08 02:08:43 +0200

    Database: Add module using the database as example

---
+++ modules/base/base/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, BigInteger
+
+from database import database, session
+
+
+class BaseBasePin(database.base):
+    __tablename__ = "base_base_pin"
+    __table_args__ = {"extend_existing": True}
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    guild_id = Column(BigInteger, unique=True)
+    limit = Column(Integer, default=5)
+
+    @staticmethod
+    def get(guild_id: int):
+        """Get userpin preferences for the guild."""
+        query = session.query(BaseBasePin).filter(BaseBasePin.guild_id == guild_id).one_or_none()
+        if query is None:
+            query = BaseBasePin(guild_id=guild_id)
+            session.add(query)
+            session.commit()
+        return query
+
+    def __repr__(self):
+        return f'<BaseBasePin guild_id="{self.guild_id}" limit="{self.limit}">'
