43a986bbe867c808301200d949570cf3b4b459fe
sinus-x <14353790+sinus-x@users.noreply.github.com>
2021-06-30 21:56:11 +0200

    Base: Add database implementation of user reaction functionalities: pinning, bookmarking and (WIP) threads (#52)
    * Base: Autopin, Autothread, Bookmarks
    
    * Base: Add strings
    
    Because raw listeners don't provide us with Context we could use for
    translation, I had to add TranslationContext object (in 'core/').
    Without it we'd have to use the global language settings and user/guild
    preference would be unusable.
    
    * Base: Split new commands into 'get' and 'set' subcommands
    
    I also altered the database functions to fix some issues
    
    * Base: Add channel overwrite support to autopin
    
    * Core: Patch 'Translator' to allow '#' sign in values
    
    * Base: Add channel overwrite support in reaction functions
    
    * Base: Add english strings
    
    * Base: Fix error logs

--- modules/base/base/database.py
+++ modules/base/base/database.py
@@ -1,26 +1,146 @@
 from __future__ import annotations
 
-from sqlalchemy import Column, Integer, BigInteger
+from typing import Optional
+
+from sqlalchemy import BigInteger, Boolean, Column, Integer
 
 from database import database, session
 
 
-class BaseBasePin(database.base):
-    __tablename__ = "base_base_pin"
+class AutoPin(database.base):
+    __tablename__ = "base_base_autopin"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    guild_id = Column(BigInteger, unique=True)
-    limit = Column(Integer, default=5)
+    guild_id = Column(BigInteger)
+    channel_id = Column(BigInteger, default=None)
+    limit = Column(Integer, default=0)
 
     @staticmethod
-    def get(guild_id: int) -> BaseBasePin:
-        """Get userpin preferences for the guild."""
-        query = session.query(BaseBasePin).filter_by(guild_id=guild_id).one_or_none()
-        if query is None:
-            query = BaseBasePin(guild_id=guild_id)
-            session.add(query)
-            session.commit()
+    def add(guild_id: int, channel_id: Optional[int], limit: int = 0) -> AutoPin:
+        """Add autopin preference."""
+        if AutoPin.get(guild_id, channel_id) is not None:
+            AutoPin.remove(guild_id, channel_id)
+        query = AutoPin(guild_id=guild_id, channel_id=channel_id, limit=limit)
+        session.add(query)
+        session.commit()
+        return query
+
+    @staticmethod
+    def get(guild_id: int, channel_id: Optional[int]) -> Optional[AutoPin]:
+        """Get autopin preferences for the guild."""
+        query = (
+            session.query(AutoPin).filter_by(guild_id=guild_id, channel_id=channel_id).one_or_none()
+        )
+        return query
+
+    @staticmethod
+    def remove(guild_id: int, channel_id: Optional[int]) -> int:
+        query = session.query(AutoPin).filter_by(guild_id=guild_id, channel_id=channel_id).delete()
         return query
 
     def __repr__(self) -> str:
-        return f'<BaseBasePin guild_id="{self.guild_id}" limit="{self.limit}">'
+        return (
+            f"<AutoPin id='{self.id}' guild_id='{self.guild_id}' "
+            f"channel_id='{self.channel_id}' limit='{self.limit}'>"
+        )
+
+    def dump(self) -> dict:
+        return {
+            "guild_id": self.guild_id,
+            "channel_id": self.channel_id,
+            "limit": self.limit,
+        }
+
+
+class AutoThread(database.base):
+    __tablename__ = "base_base_autothread"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    guild_id = Column(BigInteger)
+    channel_id = Column(BigInteger, default=None)
+    limit = Column(Integer, default=0)
+
+    @staticmethod
+    def add(guild_id: int, channel_id: Optional[int], limit: int = 0) -> AutoThread:
+        """Add autothread preference."""
+        if AutoThread.get(guild_id, channel_id) is not None:
+            AutoThread.remove(guild_id, channel_id)
+        query = AutoThread(guild_id=guild_id, channel_id=channel_id, limit=limit)
+        session.add(query)
+        session.commit()
+        return query
+
+    @staticmethod
+    def get(guild_id: int, channel_id: Optional[int]) -> Optional[AutoThread]:
+        """Get autothread preference for the guild."""
+        query = (
+            session.query(AutoThread)
+            .filter_by(guild_id=guild_id, channel_id=channel_id)
+            .one_or_none()
+        )
+        return query
+
+    @staticmethod
+    def remove(guild_id: int, channel_id: Optional[int]) -> int:
+        query = (
+            session.query(AutoThread).filter_by(guild_id=guild_id, channel_id=channel_id).delete()
+        )
+        return query
+
+    def __repr__(self) -> str:
+        return (
+            f"<AutoThread id='{self.id}' guild_id='{self.guild_id}' "
+            f"channel_id='{self.channel_id}' limit='{self.limit}'>"
+        )
+
+    def dump(self) -> dict:
+        return {
+            "guild_id": self.guild_id,
+            "channel_id": self.channel_id,
+            "limit": self.limit,
+        }
+
+
+class Bookmark(database.base):
+    __tablename__ = "base_base_bookmarks"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    guild_id = Column(BigInteger)
+    channel_id = Column(BigInteger, default=None)
+    enabled = Column(Boolean, default=False)
+
+    @staticmethod
+    def add(guild_id: int, channel_id: Optional[int], enabled: bool = False) -> Bookmark:
+        if Bookmark.get(guild_id, channel_id) is not None:
+            Bookmark.remove(guild_id, channel_id)
+        query = Bookmark(guild_id=guild_id, channel_id=channel_id, enabled=enabled)
+        session.add(query)
+        session.commit()
+        return query
+
+    @staticmethod
+    def get(guild_id: int, channel_id: Optional[int]) -> Optional[Bookmark]:
+        query = (
+            session.query(Bookmark)
+            .filter_by(guild_id=guild_id, channel_id=channel_id)
+            .one_or_none()
+        )
+        return query
+
+    @staticmethod
+    def remove(guild_id: int, channel_id: Optional[int]) -> int:
+        query = session.query(Bookmark).filter_by(guild_id=guild_id, channel_id=channel_id).delete()
+        return query
+
+    def __repr__(self) -> str:
+        return (
+            f"<Bookmark id='{self.id}' guild_id='{self.guild_id}' "
+            f"channel_id='{self.channel_id}' enabled='{self.enabled}'>"
+        )
+
+    def dump(self) -> dict:
+        return {
+            "guild_id": self.guild_id,
+            "channel_id": self.channel_id,
+            "enabled": self.enabled,
+        }
--- modules/base/base/module.py
+++ modules/base/base/module.py
@@ -3,9 +3,10 @@ import datetime
 import discord
 from discord.ext import commands
 
-from core import text, logging, utils
+from core import TranslationContext
+from core import acl, text, logging, utils
 
-from .database import BaseBasePin as Pin
+from .database import AutoPin, AutoThread, Bookmark
 
 tr = text.Translator(__file__).translate
 bot_log = logging.Bot.logger()
@@ -50,27 +51,236 @@ class Base(commands.Cog):
 
         await ctx.send(embed=embed)
 
+    @commands.guild_only()
+    @commands.check(acl.check)
+    @commands.group(name="autopin")
+    async def autopin(self, ctx):
+        await utils.Discord.send_help(ctx)
+
+    @commands.check(acl.check)
+    @autopin.command(name="get")
+    async def autopin_get(self, ctx, channel: discord.TextChannel = None):
+        embed = utils.Discord.create_embed(author=ctx.author, title=tr("autopin get", "title", ctx))
+        limit: int = getattr(AutoPin.get(ctx.guild.id, None), "limit", 0)
+        value: str = f"{limit}" if limit > 0 else tr("autopin get", "disabled", ctx)
+        embed.add_field(
+            name=tr("autopin get", "limit", ctx),
+            value=value,
+        )
+
+        if channel is None:
+            channel = ctx.channel
+
+        channel_pref = AutoPin.get(ctx.guild.id, channel.id)
+        if channel_pref is not None:
+            embed.add_field(
+                name=tr("autopin get", "channel", ctx, channel=channel.name),
+                value=f"{channel_pref.limit}"
+                if channel_pref.limit > 0
+                else tr("autopin get", "disabled", ctx),
+            )
+
+        await ctx.send(embed=embed)
+
+    @commands.check(acl.check)
+    @autopin.command(name="set")
+    async def autopin_set(self, ctx, limit: int, channel: discord.TextChannel = None):
+        """Set autopin limit."""
+        if limit < 1:
+            raise commands.ArgumentError("Limit has to be at least one.")
+
+        if channel is None:
+            AutoPin.add(ctx.guild.id, None, limit)
+            await guild_log.info(
+                ctx.author,
+                ctx.channel,
+                f"Global autopin limit set to {limit}.",
+            )
+        else:
+            AutoPin.add(ctx.guild.id, channel.id, limit)
+            await guild_log.info(
+                ctx.author,
+                ctx.channel,
+                f"#{channel.name} autopin limit set to {limit}.",
+            )
+
+        if limit == 0:
+            await ctx.reply(tr("autopin set", "disabled", ctx))
+        else:
+            await ctx.reply(tr("autopin set", "reply", ctx))
+
+    @commands.check(acl.check)
+    @autopin.command(name="unset")
+    async def autopin_unset(self, ctx, channel: discord.TextChannel = None):
+        if channel is None:
+            AutoPin.remove(ctx.guild.id, None)
+            await guild_log.info(ctx.author, ctx.channel, "Autopin unset globally.")
+        else:
+            AutoPin.remove(ctx.guild.id, channel.id)
+            await guild_log.info(ctx.author, ctx.channel, f"Autopin unset in #{channel.name}.")
+        await ctx.reply(tr("autopin unset", "reply"))
+
+    @commands.guild_only()
+    @commands.check(acl.check)
+    @commands.group(name="bookmarks")
+    async def bookmarks(self, ctx):
+        await utils.Discord.send_help(ctx)
+
+    @commands.check(acl.check)
+    @bookmarks.command(name="get")
+    async def bookmarks_get(self, ctx, channel: discord.TextChannel = None):
+        embed = utils.Discord.create_embed(
+            author=ctx.author, title=tr("bookmarks get", "title", ctx)
+        )
+        enabled: int = getattr(Bookmark.get(ctx.guild.id, None), "enabled", False)
+        embed.add_field(
+            name=tr("bookmarks get", "settings", ctx),
+            value=tr("bookmarks get", str(enabled), ctx),
+        )
+
+        if channel is None:
+            channel = ctx.channel
+
+        channel_pref = Bookmark.get(ctx.guild.id, channel.id)
+        if channel_pref is not None:
+            embed.add_field(
+                name=tr("bookmarks get", "channel", ctx, channel=channel.name),
+                value=tr("bookmarks get", str(channel_pref.enabled), ctx),
+            )
+
+        await ctx.send(embed=embed)
+
+    @commands.check(acl.check)
+    @bookmarks.command(name="set")
+    async def bookmarks_set(self, ctx, enabled: bool, channel: discord.TextChannel = None):
+        """Enable or disable bookmarking."""
+        if channel is None:
+            Bookmark.add(ctx.guild.id, None, enabled)
+            await guild_log.info(ctx.author, ctx.channel, f"Global bookmarks set to {enabled}.")
+        else:
+            Bookmark.add(ctx.guild.id, channel.id, enabled)
+            await guild_log.info(
+                ctx.author, ctx.channel, f"#{channel.name} bookmarks set to {enabled}."
+            )
+        await ctx.reply(tr("bookmarks set", str(enabled), ctx))
+
+    @commands.check(acl.check)
+    @bookmarks.command(name="unset")
+    async def bookmarks_unset(self, ctx, channel: discord.TextChannel = None):
+        """Remove bookmark settings."""
+        if channel is None:
+            Bookmark.remove(ctx.guild.id, None)
+            await guild_log.info(ctx.author, ctx.channel, "Bookmarking unset globally.")
+        else:
+            Bookmark.remove(ctx.guild.id, channel.id)
+            await guild_log.info(ctx.author, ctx.channel, f"Bookmarking unset in #{channel.name}.")
+        await ctx.reply(tr("bookmarks unset", "reply"))
+
+    @commands.guild_only()
+    @commands.check(acl.check)
+    @commands.group(enabled=False)
+    async def autothread(self, ctx):
+        await utils.Discord.send_help(ctx)
+
+    @commands.check(acl.check)
+    @autothread.command(name="get")
+    async def autothread_get(self, ctx, channel: discord.TextChannel = None):
+        embed = utils.Discord.create_embed(
+            author=ctx.author, title=tr("autothread get", "title", ctx)
+        )
+        limit: int = getattr(AutoThread.get(ctx.guild.id, None), "limit", 0)
+        value: str = f"{limit}" if limit > 0 else tr("autothread get", "disabled", ctx)
+        embed.add_field(
+            name=tr("autothread get", "limit", ctx),
+            value=value,
+        )
+
+        if channel is None:
+            channel = ctx.channel
+
+        channel_pref = AutoThread.get(ctx.guild.id, channel.id)
+        if channel_pref is not None:
+            embed.add_field(
+                name=tr("autothread get", "channel", ctx, channel=channel.name),
+                value=f"{channel_pref.limit}"
+                if channel_pref.limit > 0
+                else tr("autothread get", "disabled", ctx),
+            )
+
+        await ctx.send(embed=embed)
+
+    @commands.check(acl.check)
+    @autothread.command(name="set")
+    async def autothread_set(self, ctx, limit: int, channel: discord.TextChannel = None):
+        if limit < 1:
+            raise commands.ArgumentError("Limit has to be at least one.")
+
+        if channel is None:
+            AutoThread.add(ctx.guild.id, None, limit)
+            await guild_log.info(
+                ctx.author,
+                ctx.channel,
+                f"Global autothread limit set to {limit}.",
+            )
+        else:
+            AutoThread.add(ctx.guild.id, channel.id, limit)
+            await guild_log.info(
+                ctx.author,
+                ctx.channel,
+                f"#{channel.name} autothread limit set to {limit}.",
+            )
+
+        if limit == 0:
+            await ctx.reply(tr("autothread set", "disabled", ctx))
+        else:
+            await ctx.reply(tr("autothread set", "reply", ctx))
+
+    @commands.check(acl.check)
+    @autothread.command(name="unset")
+    async def autothread_unset(self, ctx, channel: discord.TextChannel = None):
+        if channel is None:
+            AutoThread.remove(ctx.guild.id, None)
+            await guild_log.info(ctx.author, ctx.channel, "Autothread unset globally.")
+        else:
+            AutoThread.remove(ctx.guild.id, channel.id)
+            await guild_log.info(ctx.author, ctx.channel, f"Autothread unset in #{channel.name}.")
+        await ctx.reply(tr("autothread unset", "reply"))
+
     #
 
-    # TODO Move this to separate module in some optional repo
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         """Handle message pinning."""
+        if payload.guild_id is None or payload.member is None:
+            return
+
         emoji = getattr(payload.emoji, "name", None)
-        if emoji != "📌":
+        if emoji == "📌" or emoji == "📍":
+            await self._autopin(payload, emoji)
+        elif emoji == "🔖":
+            await self._bookmark(payload)
+        elif emoji == "🧵":
+            await self._autothread(payload)
+
+    async def _autopin(self, payload: discord.RawReactionActionEvent, emoji: str):
+        """Handle autopin functionality."""
+        tc = TranslationContext(payload.guild_id, payload.user_id)
+
+        message = await utils.Discord.get_message(
+            self.bot, payload.guild_id, payload.channel_id, payload.message_id
+        )
+        if message is None:
+            await bot_log.error(
+                payload.member,
+                None,
+                "Could not find message "
+                + utils.Discord.message_url_from_reaction_payload(payload),
+            )
             return
 
-        if getattr(payload, "guild_id", None) is None:
-            return
-
-        channel = self.bot.get_channel(payload.channel_id)
-        if type(channel) != discord.TextChannel:
-            return
-
-        try:
-            message: discord.Message = await channel.fetch_message(payload.message_id)
-        except discord.errors.HTTPException as exc:
-            await guild_log.error(None, channel, f"Could not find message while pinnig: {exc}.")
+        if emoji == "📍" and not payload.member.bot:
+            await payload.member.send(tr("_autopin", "bad pin emoji", tc))
+            await utils.Discord.remove_reaction(message, emoji, payload.member)
             return
 
         for reaction in message.reactions:
@@ -78,33 +288,118 @@ class Base(commands.Cog):
                 continue
 
             # remove if the message is pinned or is in unpinnable channel
-            # TODO Unpinnable channels
             if message.pinned:
                 await guild_log.debug(
-                    None,
-                    channel,
+                    payload.member,
+                    message.channel,
                     f"Removing {payload.user_id}'s pin: Message is already pinned.",
                 )
                 await reaction.clear()
                 return
 
-            # stop if there is not enough pins
-            if reaction.count < Pin().get(payload.guild_id).limit:
+            # stop if there isn't enough pins
+            limit: int = getattr(AutoPin.get(payload.guild_id, payload.channel_id), "limit", -1)
+            # overwrite for channel doesn't exist, use guild preference
+            if limit < 0:
+                limit = getattr(AutoPin.get(payload.guild_id, None), "limit", 0)
+            if limit == 0 or reaction.count < limit:
                 return
 
             try:
                 await message.pin()
                 await guild_log.info(
-                    None,
-                    channel,
-                    f"Pinning message {message.id} by {message.author.name}.",
+                    payload.member,
+                    message.channel,
+                    f"Pinned message {message.jump_url}",
                 )
             except discord.errors.HTTPException:
-                await guild_log.error(None, channel, "Could not pin message.")
+                await guild_log.error(payload.member, message.channel, "Could not pin message.")
                 return
 
             await reaction.clear()
             await message.add_reaction("📍")
+
+    async def _bookmark(self, payload: discord.RawReactionActionEvent):
+        """Handle bookmark functionality."""
+        if not Bookmark.get(payload.guild_id).enabled:
+            return
+
+        tc = TranslationContext(payload.guild_id, payload.user_id)
+
+        message = await utils.Discord.get_message(
+            self.bot, payload.guild_id, payload.channel_id, payload.message_id
+        )
+        if message is None:
+            await bot_log.error(
+                payload.member,
+                None,
+                "Could not find message "
+                + utils.Discord.message_url_from_reaction_payload(payload),
+            )
+            return
+
+        embed = utils.Discord.create_embed(
+            author=payload.member,
+            title=tr("_bookmark", "title", tc),
+            description=message.content[:2000],
+        )
+        embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
+
+        timestamp = utils.Time.datetime(message.created_at)
+        embed.add_field(
+            name=f"{timestamp} UTC",
+            value=tr(
+                "_bookmark",
+                "info",
+                tc,
+                guild=utils.Text.sanitise(message.guild.name),
+                channel=utils.Text.sanitise(message.channel.name),
+                link=message.jump_url,
+            ),
+            inline=False,
+        )
+
+        if len(message.attachments):
+            embed.add_field(
+                name=tr("_bookmark", "files", tc),
+                value=tr("_bookmark", "total", tc, count=len(message.attachments)),
+            )
+        if len(message.embeds):
+            embed.add_field(
+                name=tr("_bookmark", "embeds", tc),
+                value=tr("_bookmark", "total", tc, count=len(message.embeds)),
+            )
+
+        await utils.Discord.remove_reaction(message, payload.emoji, payload.member)
+        await payload.member.send(embed=embed)
+
+        await guild_log.debug(
+            payload.member, message.channel, f"Bookmarked message {message.jump_url}."
+        )
+
+    async def _autothread(self, payload: discord.RawReactionActionEvent):
+        """Handle autothread functionality.
+
+        This function is not yet available on Discord publicly, nor in in
+        discord.py, so it doesn't do anything.
+        """
+        tc = TranslationContext(payload.guild_id, payload.user_id)
+
+        message = await utils.Discord.get_message(
+            self.bot, payload.guild_id, payload.channel_id, payload.message_id
+        )
+        if message is None:
+
+            await bot_log.error(
+                payload.member,
+                None,
+                "Could not find message "
+                + utils.Discord.message_url_from_reaction_payload(payload),
+            )
+            return
+
+        await utils.Discord.remove_reaction(message, payload.emoji, payload.member)
+        await payload.member.send(tr("_autothread", "wip", tc))
 
 
 def setup(bot) -> None:
