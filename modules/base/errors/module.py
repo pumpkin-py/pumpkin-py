40365283f8182afdd55a956115627a2a34460a81
sinus-x <14353790+sinus-x@users.noreply.github.com>
2021-07-20 23:40:24 +0200

    Errors: Improve error logging
    - Replace traceback in error handler with user-friendly error content
      only
    - Simplify __get_error_message by only outputting one boolean
      instead of two. If it is set to 'True', the error content is shown
      to the user AND a log entry is created. If set to 'False', it is
      just user's mistake (missing argument, permission issue).
    - Logging: Include traceback in the format_stderr() and format_discord()
      if it is included in 'extra' attribute.

--- modules/base/errors/module.py
+++ modules/base/errors/module.py
@@ -46,47 +46,40 @@ class Errors(commands.Cog):
             return
 
         # Get information
-        title, content, show_traceback, inform = Errors.__get_error_message(ctx, error)
+        title, content, log_error = Errors.__get_error_message(ctx, error)
         embed = utils.Discord.create_embed(
             author=ctx.author, error=True, title=title, description=content
         )
-
-        tb: str = "".join(
-            traceback.format_exception(type(error), error, error.__traceback__)
-        )
-
-        if show_traceback:
+        if log_error:
             embed.add_field(
-                name="Traceback", value="```" + tb[-256:] + "```", inline=False
+                name=tr("on_command_error", "error content"),
+                value=str(error),
+                inline=False,
             )
         await ctx.send(embed=embed)
 
-        # Send to error logging channel
-        if inform:
-            # TODO Complete when we know what channel we have to send the content to
-            pass
-
         # Log the error
-        if show_traceback or inform:
-            await guild_log.error(
+        if log_error:
+            await bot_log.error(
                 ctx.author,
                 ctx.channel,
                 f"{type(error).__name__}: {str(error)}",
-                traceback=traceback.format_tb(error.__traceback__),
+                traceback=traceback.format_exception(
+                    type(error), error, error.__traceback__
+                ),
             )
 
     def __get_error_message(
         ctx: commands.Context, error: Exception
-    ) -> Tuple[str, str, bool, bool]:
+    ) -> Tuple[str, str, bool]:
         """Get message for the error.
 
         :param ctx: The invocation context.
         :param error: Detected exception.
         :return:
-            title (The error name),
-            content (The error description),
-            show_traceback (Whether to display traceback),
-            inform (Whether to send the error for further inspection).
+            title (Translated error name),
+            content (Translated description),
+            write_tb (Whether to display traceback in the log),
         """
 
         # pumpkin.py own exceptions
@@ -94,7 +87,6 @@ class Errors(commands.Cog):
             return (
                 tr("pumpkin.py", type(error).__name__, ctx),
                 str(error),
-                False,
                 True,
             )
 
@@ -104,14 +96,12 @@ class Errors(commands.Cog):
                 tr("MissingRequiredArgument", "name", ctx),
                 tr("MissingRequiredArgument", "value", ctx, arg=error.param.name),
                 False,
-                False,
             )
         if type(error) == commands.CommandOnCooldown:
             time = utils.Time.seconds(error.retry_after)
             return (
                 tr("CommandOnCooldown", "name", ctx),
                 tr("CommandOnCooldown", "value", ctx, time=time),
-                False,
                 False,
             )
         if type(error) == commands.MaxConcurrencyReached:
@@ -125,20 +115,17 @@ class Errors(commands.Cog):
                     per=error.per.name,
                 ),
                 False,
-                False,
             )
         if type(error) == commands.MissingRole:
             return (
                 tr("MissingRole", "name", ctx),
                 tr("MissingRole", "value", ctx, role=f"`{error.missing_role!r}`"),
                 False,
-                False,
             )
         if type(error) == commands.BotMissingRole:
             return (
                 tr("BotMissingRole", "name", ctx),
                 tr("BotMissingRole", "value", ctx, role=f"`{error.missing_role!r}`"),
-                False,
                 False,
             )
         if type(error) == commands.MissingAnyRole:
@@ -147,14 +134,12 @@ class Errors(commands.Cog):
                 tr("MissingAnyRole", "name", ctx),
                 tr("MissingAnyRole", "value", ctx, roles=roles),
                 False,
-                False,
             )
         if type(error) == commands.BotMissingAnyRole:
             roles = ", ".join(f"**{r!r}**" for r in error.missing_roles)
             return (
                 tr("BotMissingAnyRole", "name", ctx),
                 tr("BotMissingAnyRole", "value", ctx, roles=roles),
-                False,
                 False,
             )
         if type(error) == commands.MissingPermissions:
@@ -163,7 +148,6 @@ class Errors(commands.Cog):
                 tr("MissingPermissions", "name", ctx),
                 tr("MissingPermissions", "value", ctx, perms=perms),
                 False,
-                False,
             )
         if type(error) == commands.BotMissingPermissions:
             perms = ", ".join(f"`{p}`" for p in error.missing_perms)
@@ -171,20 +155,17 @@ class Errors(commands.Cog):
                 tr("BotMissingPermissions", "name", ctx),
                 tr("BotMissingPermissions", "value", ctx, perms=perms),
                 False,
-                False,
             )
         if type(error) == commands.BadUnionArgument:
             return (
                 tr("BadUnionArgument", "name", ctx),
                 tr("BadUnionArgument", "value", ctx, param=error.param.name),
                 False,
-                False,
             )
         if type(error) == commands.BadBoolArgument:
             return (
                 tr("BadBoolArgument", "name", ctx),
                 tr("BadBoolArgument", "value", ctx, arg=error.argument),
-                False,
                 False,
             )
         if type(error) == commands.ConversionError:
@@ -197,7 +178,6 @@ class Errors(commands.Cog):
                     converter=type(error.converter).__name__,
                 ),
                 False,
-                False,
             )
 
         # extensions
@@ -207,7 +187,6 @@ class Errors(commands.Cog):
             return (
                 tr("ExtensionFailed", "name", ctx),
                 tr("ExtensionFailed", "value", ctx, extension=name),
-                True,
                 True,
             )
         if isinstance(error, commands.ExtensionError):
@@ -222,7 +201,6 @@ class Errors(commands.Cog):
                 tr(key, "name", ctx),
                 tr(key, "value", ctx, extension=name),
                 False,
-                False,
             )
 
         # the rest of client exceptions
@@ -231,7 +209,6 @@ class Errors(commands.Cog):
                 tr("CommandRegistrationError", "name", ctx),
                 tr("CommandRegistrationError", "value", ctx, cmd=error.name),
                 False,
-                False,
             )
         if isinstance(error, commands.CommandError) or isinstance(
             error, discord.ClientException
@@ -239,7 +216,6 @@ class Errors(commands.Cog):
             return (
                 tr(type(error).__name__, "name", ctx),
                 tr(type(error).__name__, "value", ctx),
-                False,
                 True,
             )
 
@@ -250,7 +226,6 @@ class Errors(commands.Cog):
             return (
                 tr(type(error).__name__, "name", ctx),
                 tr(type(error).__name__, "value", ctx),
-                False,
                 True,
             )
 
@@ -260,14 +235,12 @@ class Errors(commands.Cog):
                 tr(type(error).__name__, "name", ctx),
                 tr(type(error).__name__, "value", ctx),
                 True,
-                True,
             )
 
         # other exceptions
         return (
             type(error).__name__,
             tr("Unknown", "value", ctx),
-            True,
             True,
         )
 
