48e7fd15d088bcf6f5ae457e87af1cc923f9154b
sinus-x <14353790+sinus-x@users.noreply.github.com>
2022-01-20 22:31:04 +0100

    Core: Add '_tracing' module
    See the commit content for mode details.

---
+++ pie/_tracing/__init__.py
@@ -0,0 +1,58 @@
+import os
+from typing import Callable, Tuple
+
+
+sources: Tuple[str] = ("pie_spamchannel",)
+
+
+def register(name: str) -> Callable:
+    """Register pumpkin.py tracing function.
+
+    This is 'the last resort' option when debugging entangled code.
+
+    Some parts of the code are difficult to debug, by design. One of them being
+    spamchannel, for example. For these times, this can be helpful as
+    'permanent' solution that stays idle most of the time.
+
+    The usage is like so:
+
+    .. code-block:
+
+        _trace: Callable = pie._tracing.register_trace_function("feature")
+
+    The 'feature' must be registered in 'pie._tracing', otherwise 'ValueError'
+    is raised.
+
+    This feature may be enabled by altering environment variables:
+
+    .. code-block:
+
+        trace_pie_spamchannel=1 python3 pumpkin.py
+
+    The registration name MUST follow the directory structure:
+    'pie_spamchannel', 'modules_base_errors', ...
+
+    When everything goes well, you'll see debug logs printed to the stdout:
+
+    .. code-block:
+
+        Imported database models in 'pie.acl.database'.
+        Imported database models in 'pie.i18n.database'.
+        Imported database models in 'pie.logger.database'.
+        [trace:pie_spamchannel] Function registered.
+        Imported database models in 'pie.spamchannel.database'.
+        ...
+    """
+    prefix: str = f"[trace:{name}]"
+
+    if name not in sources:
+        raise ValueError(f"Unregistered tracing source '{name}'.")
+
+    if not os.getenv(f"trace_{name}"):
+        return lambda *args, **kwargs: None
+
+    def _trace(message: str) -> None:
+        print("{prefix} {message}".format(prefix=prefix, message=message))
+
+    _trace("Function registered.")
+    return _trace
