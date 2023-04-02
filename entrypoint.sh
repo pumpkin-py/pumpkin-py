e952ecdec779991d10af78724f6ad5a72da50eb0
czechbol <adamludes@gmail.com>
2023-04-02 11:37:12 +0200

    env update

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,17 +9,17 @@ else
     git clone https://github.com/pumpkin-py/pumpkin-py.git /pumpkin-py || (echo "Couldn't clone pumpkin-py repository. Volume /pumpkin-py isn't empty and it doesn't contain pumpkin-py repository." && exit 1)
 fi
 
-if [ -z "${TZ}" ]; then
+if [ -z "${BOT_TIMEZONE}" ]; then
     echo "TZ is not set, using UTC instead"
-    ln -snf /usr/share/zoneinfo/Etc/UTC /etc/localtime && echo $TZ > /etc/timezone
+    ln -snf /usr/share/zoneinfo/Etc/UTC /etc/localtime && echo $BOT_TIMEZONE > /etc/timezone
 else
-    echo "TZ is set to $TZ"
-    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+    echo "TZ is set to $BOT_TIMEZONE"
+    ln -snf /usr/share/zoneinfo/$BOT_TIMEZONE /etc/localtime && echo $BOT_TIMEZONE > /etc/timezone
 fi
 
-if [ -n "${EXTRA_PKGS}" ]; then
-    echo "Installing extra packages: $EXTRA_PKGS"
-    apt-get -y --no-install-recommends install $EXTRA_PKGS
+if [ -n "${BOT_EXTRA_PACKAGES}" ]; then
+    echo "Installing extra packages: $BOT_EXTRA_PACKAGES"
+    apt-get -y --no-install-recommends install $BOT_EXTRA_PACKAGES
 fi
 
 mkdir -p /tempdir
